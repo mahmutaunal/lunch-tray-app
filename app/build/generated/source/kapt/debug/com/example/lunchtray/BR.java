e5c7d07ccedbf0289aa0e70925dc59f774f89d54
Mahmut Alperen Ünal <44277681+mahmutaunal@users.noreply.github.com>
2023-03-30 13:03:00 +0300

    initial commit

---
+++ app/build/generated/source/kapt/debug/com/example/lunchtray/BR.java
@@ -0,0 +1,15 @@
+package com.example.lunchtray;
+
+public class BR {
+  public static final int AccompanimentMenuFragment = 1;
+
+  public static final int EntreeMenuFragment = 2;
+
+  public static final int SideMenuFragment = 3;
+
+  public static final int _all = 0;
+
+  public static final int checkoutFragment = 4;
+
+  public static final int viewModel = 5;
+}
